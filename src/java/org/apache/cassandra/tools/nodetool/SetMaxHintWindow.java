437c659f13ad430c3cff260aa94ea3f706e464f5
hnishi <hnishi@yahoo-corp.jp>
2017-05-03 10:24:27 +0900

    Modified description of  nodetool setmaxhintwindow.

--- src/java/org/apache/cassandra/tools/nodetool/SetMaxHintWindow.java
+++ src/java/org/apache/cassandra/tools/nodetool/SetMaxHintWindow.java
@@ -26,7 +26,7 @@ import org.apache.cassandra.tools.NodeTool;
 @Command(name = "setmaxhintwindow", description = "Set the specified max hint window in ms")
 public class SetMaxHintWindow extends NodeTool.NodeToolCmd
 {
-    @Arguments(title = "max_hint_window", usage = "<value_in_ms>", description = "Value", required = true)
+    @Arguments(title = "max_hint_window", usage = "<value_in_ms>", description = "Value of maxhintwindow in ms", required = true)
     private Integer maxHintWindow = null;
 
     @Override
